fa89e80c91ca286b21d4179e7927d09d5771d5f5
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-12 22:06:53 +0200

    git: Updated EOL & paths for WSL

--- main.rb
+++ main.rb
@@ -24,35 +24,35 @@ OUTPUT_DIR = ARGV[1] unless ARGV[1].nil?
 OUTPUT_BEST_SOLUTIONS = ARGV[2] unless ARGV[2].nil?
 
 LEVELS = {
-  "PB000" => { :name => "trash-world-news", :order => 1 }, 
-  "PB001" => { :name => "trash-world-news", :order => 2 }, 
-  "PB037" => { :name => "trash-world-news", :order => 3 }, 
-  "PB002" => { :name => "trash-world-news", :order => 4 }, 
-  "PB003B" => { :name => "euclids-pizza", :order => 5 }, 
-  "PB004" => { :name => "mitsuzen-hdi10", :order => 6 }, 
-  "PB005" => { :name => "last-stop-snaxnet", :order => 7 }, 
-  "PB006B" => { :name => "zebros-copies", :order => 8 }, 
-  "PB007" => { :name => "sfcta-highway-sign-#4902", :order => 9 }, 
-  "PB008" => { :name => "unknown-network-1", :order => 10 }, 
-  "PB009" => { :name => "uc-berkeley", :order => 11 }, 
-  "PB010B" => { :name => "workhouse", :order => 12 }, 
+  "PB000" => { :name => "trash-world-news", :order => 1 },
+  "PB001" => { :name => "trash-world-news", :order => 2 },
+  "PB037" => { :name => "trash-world-news", :order => 3 },
+  "PB002" => { :name => "trash-world-news", :order => 4 },
+  "PB003B" => { :name => "euclids-pizza", :order => 5 },
+  "PB004" => { :name => "mitsuzen-hdi10", :order => 6 },
+  "PB005" => { :name => "last-stop-snaxnet", :order => 7 },
+  "PB006B" => { :name => "zebros-copies", :order => 8 },
+  "PB007" => { :name => "sfcta-highway-sign-#4902", :order => 9 },
+  "PB008" => { :name => "unknown-network-1", :order => 10 },
+  "PB009" => { :name => "uc-berkeley", :order => 11 },
+  "PB010B" => { :name => "workhouse", :order => 12 },
   "PB012" => { :name => "equity-first-bank", :order => 13 },
-  "PB011B" => { :name => "mitsuzen-hdi10", :order => 14 },  
+  "PB011B" => { :name => "mitsuzen-hdi10", :order => 14 },
   "PB013C" => { :name => "trash-world-news", :order => 15 },
-  "PB014" => { :name => "kgogtv", :order => 16 }, 
-  "PB015" => { :name => "tec-redshift", :order => 17 }, 
+  "PB014" => { :name => "kgogtv", :order => 16 },
+  "PB015" => { :name => "tec-redshift", :order => 17 },
   "PB016" => { :name => "digital-library-project", :order => 18 },
   "PB040" => { :name => "tec-exablaster-mode", :order => 19 },
-  "PB018" => { :name => "emersons-guide", :order => 20 }, 
-  "PB027" => { :name => "valhalla", :order => 21 }, 
-  "PB038" => { :name => "mitsuzen-hdi10", :order => 22 }, 
-  "PB020" => { :name => "sawayama-wonderdisc", :order => 23 }, 
-  "PB021" => { :name => "alliance-power-and-light", :order => 24 }, 
-  "PB022" => { :name => "deadlocks-domain", :order => 25 }, 
+  "PB018" => { :name => "emersons-guide", :order => 20 },
+  "PB027" => { :name => "valhalla", :order => 21 },
+  "PB038" => { :name => "mitsuzen-hdi10", :order => 22 },
+  "PB020" => { :name => "sawayama-wonderdisc", :order => 23 },
+  "PB021" => { :name => "alliance-power-and-light", :order => 24 },
+  "PB022" => { :name => "deadlocks-domain", :order => 25 },
   "PB023" => { :name => "xtreme-league-baseball", :order => 26 },
-  "PB024" => { :name => "kings-ransom-online", :order => 27 }, 
-  "PB028" => { :name => "kgogtv", :order => 28 }, 
-  "PB025" => { :name => "equity-first-bank", :order => 29 }, 
+  "PB024" => { :name => "kings-ransom-online", :order => 27 },
+  "PB028" => { :name => "kgogtv", :order => 28 },
+  "PB025" => { :name => "equity-first-bank", :order => 29 },
   "PB026B" => { :name => "tec-exablaster-modem", :order => 30 },
   "PB029B" => { :name => "last-stop-snaxnet", :order => 31 },
   "PB030" => { :name => "mitsuzen-hdi10", :order => 32 },
@@ -158,7 +158,7 @@ def write_solutions_markdown(best_solutions)
 
   best_solutions.each do | level_id, v |
     # puts level_id
-    # puts 
+    # puts
     # puts v.to_s
     file_best.puts("| #{level_id} | #{LEVELS[level_id][:name]} | #{v[:cycles]} | #{v[:size]} | #{v[:activity]} | &nbsp; | `#{v[:best_by_cycles]}` | `#{v[:best_by_size]}` | `#{v[:best_by_activity]}` |")
   end
